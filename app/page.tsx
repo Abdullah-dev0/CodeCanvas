dce24b050c0056b1b1d26ed241ca13c24c597391
Abdullah-142 <abdulah14200@gmail.com>
2024-05-23 11:15:30 -0700

    chore: Update dependencies and configuration files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+
+import { FileUploader } from "@/components/shared/FileUploader";
 import { ModeToggle } from "@/components/theme/Toggle";
 
 const HomePage = () => {
    return (
-      <div className="text-center  h-screen">
-         {/* <ModeToggle /> */}
+      <div className="text-center grid place-items-center gap-5  h-screen">
+         <ModeToggle />
+         <FileUploader
+            imageUrl="https://images.unsplash.com/photo-1575936123452-b67c3203c357?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+            onFieldChange={() => {}}
+            setFiles={() => {}}
+         />
       </div>
    );
 };
