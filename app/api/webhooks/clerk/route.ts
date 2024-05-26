b301a0a1b87717614c5fa4e6e3763a23c2b010c2
Abdullah-142 <abdulah14200@gmail.com>
2024-05-26 05:30:13 -0700

    Refactor webhook route to include created_at and updated_at fields in user object

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,4 +1,4 @@
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { createUser, deleteUser, updateUser } from "@/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { User } from "@prisma/client";
 import { headers } from "next/headers";
@@ -93,21 +93,24 @@ export async function POST(req: Request) {
          image_url,
          first_name,
          last_name,
+         email_addresses,
          username,
          created_at,
          updated_at,
       } = evt.data;
 
       const user = {
+         clerkId: id,
+         email: email_addresses[0].email_address,
+         username: username!,
          firstName: first_name,
          lastName: last_name,
-         username: username!,
-         photo: image_url,
+         image: image_url,
          createdAt: created_at,
          updatedAt: updated_at,
       };
 
-      const updatedUser = await updateUser(user, id);
+      const updatedUser = await updateUser(user as User, id);
 
       return NextResponse.json({ message: "OK", user: updatedUser });
    }
