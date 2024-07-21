847e5ad68eab87b79b0ba2729d352984d36d220b
Abdullah-142 <abdulah14200@gmail.com>
2024-07-22 03:00:14 +0500

    chore: Refactor SelectOption

--- components/shared/SelectOption.tsx
+++ components/shared/SelectOption.tsx
@@ -1,62 +1,45 @@
-import {
-   Select,
-   SelectContent,
-   SelectItem,
-   SelectTrigger,
-   SelectValue,
-} from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { frameworks, styles, useCases } from "@/constant";
 
 type SelectOptionProps = {
-   type: string;
-   state?: boolean;
-   placeholder: string;
-   onChangeHandler?: (value: string) => void;
-   value: string | undefined;
+	type: string;
+	state?: boolean;
+	placeholder: string;
+	onChangeHandler?: (value: string) => void;
+	value: string | undefined;
 };
 
-const SelectOption = ({
-   type,
-   placeholder,
-   onChangeHandler,
-   value,
-   state,
-}: SelectOptionProps) => {
-   let options: any = [];
+const SelectOption = ({ type, placeholder, onChangeHandler, value, state }: SelectOptionProps) => {
+	let options: any = [];
 
-   switch (type) {
-      case "framework":
-         options = frameworks;
-         break;
-      case "style":
-         options = styles;
-         break;
-      case "usecase":
-         options = useCases;
-         break;
-      default:
-         options = [];
-   }
+	switch (type) {
+		case "framework":
+			options = frameworks;
+			break;
+		case "style":
+			options = styles;
+			break;
+		case "usecase":
+			options = useCases;
+			break;
+		default:
+			options = [];
+	}
 
-   return (
-      <Select
-         onValueChange={onChangeHandler}
-         disabled={state}
-         defaultValue={value}
-         value={value}
-      >
-         <SelectTrigger>
-            <SelectValue placeholder={placeholder} />
-         </SelectTrigger>
-         <SelectContent side="top" position="popper">
-            {options.map((option: any) => (
-               <SelectItem key={option.id} value={option.name}>
-                  {option.name}
-               </SelectItem>
-            ))}
-         </SelectContent>
-      </Select>
-   );
+	return (
+		<Select onValueChange={onChangeHandler} disabled={state} defaultValue={value} value={value}>
+			<SelectTrigger>
+				<SelectValue placeholder={placeholder} />
+			</SelectTrigger>
+			<SelectContent side="top" position="popper">
+				{options.map((option: any) => (
+					<SelectItem key={option.id} value={option.name}>
+						{option.name}
+					</SelectItem>
+				))}
+			</SelectContent>
+		</Select>
+	);
 };
 
 export default SelectOption;
